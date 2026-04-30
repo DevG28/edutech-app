6803beb1b740c2f21e031b30bdddc34e40318eb4
DevG28 <codepython22@gmail.com>
2026-04-29 23:10:48 -0500

    Base navigation y data structure listo

--- app/src/main/java/com/edutech/app/navigation/NavGraph.kt
+++ app/src/main/java/com/edutech/app/navigation/NavGraph.kt
@@ -1,2 +1,48 @@
 package com.edutech.app.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+//import com.edutech.app.ui.screens.LoginScreen
+//import com.edutech.app.ui.screens.HomeScreen
+
+// Definición de rutas
+object Routes {
+    const val LOGIN = "login"
+    const val HOME = "home"
+    const val COURSES = "courses"
+    const val DETAIL = "detail"
+    const val PROFILE = "profile"
+}
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.LOGIN
+    ) {
+
+        composable(Routes.LOGIN) {
+            LoginScreen(navController)
+        }
+
+        composable(Routes.HOME) {
+            HomeScreen(navController)
+        }
+
+        composable(Routes.COURSES) {
+            // Luego CoursesScreen(navController)
+        }
+
+        composable("${Routes.DETAIL}/{courseId}") {
+            val courseId = it.arguments?.getString("courseId")
+            // Luego DetailScreen(courseId)
+        }
+
+        composable(Routes.PROFILE) {
+            // Luego ProfileScreen(navController)
+        }
+    }
+}
